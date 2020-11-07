53b12d6f539b818da7bdc329b6c777e3422e55fe
Pradyumna Mahajan <65710156+pradyumnamahajan52@users.noreply.github.com>
2020-11-07 18:38:08 +0530

    Add files via upload

--- socialshout/urls.py
+++ socialshout/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('subproblems/<str:mainproblem_name>/<int:id>/', views.subproblems, name="subproblems"),
+    path('problems_form/<str:mainproblem_name>/<int:main_id>/<str:subproblem_name>/<int:sub_id>/', views.subproblems_form, name="subproblems_form"),
 ]
--- socialshout/views.py
+++ socialshout/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
-
+from complaint.models import Complaint_Category,Complaint_Subcategory
 # Create your views here.
 
 def index(request):
-    return render(request,'socialShout/index.html')
+    complaint_category = Complaint_Category.objects.all()
+    params = {
+        'complaint_category':complaint_category
+    }
+    return render(request,'socialShout/index.html',params)
+
+def subproblems(request,mainproblem_name,id):
+    complaint_subcategory = Complaint_Subcategory.objects.filter(complaint_category=id).all()
+    params = {
+        'complaint_subcategory':complaint_subcategory,
+    }
+    return render(request,'socialShout/subproblems.html',params)
+
+def subproblems_form(request,mainproblem_name,main_id,subproblem_name,sub_id):
+    params = {
+        'mainproblem_name':mainproblem_name,
+        'subproblem_name':subproblem_name,   
+        'main_id':main_id,
+    }
+    return render(request,'socialShout/problemsform.html',params)
