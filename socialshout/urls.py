74f39befba135014eca7d2bede940e655e623478
Pradyumna Mahajan <65710156+pradyumnamahajan52@users.noreply.github.com>
2020-11-08 10:10:19 +0530

    socialshout

--- socialshout/urls.py
+++ socialshout/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('subproblems/<str:mainproblem_name>/<int:id>/', views.subproblems, name="subproblems"),
     path('problems_form/<str:mainproblem_name>/<int:main_id>/<str:subproblem_name>/<int:sub_id>/', views.subproblems_form, name="subproblems_form"),
     path('verify_otp/', views.verify_otp, name="verify_otp"),
-    path('problems_form_submit/', views.problems_form_submit, name="problems_form_submit"),
+    path('problems_form_submit/<int:main_id>/<int:sub_id>/', views.problems_form_submit, name="problems_form_submit"),
 
 ]
--- socialshout/views.py
+++ socialshout/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
-from complaint.models import Complaint_Category,Complaint_Subcategory
+from django.shortcuts import render,redirect
+from complaint.models import Complaint_Category,Complaint_Subcategory,Complaint_User
+from django.contrib import messages
+from django.db import IntegrityError
+from django.core.mail import send_mail
+from django.core.exceptions import ValidationError
+from datetime import datetime,timedelta
+import math
+import random
+
 # Create your views here.
 
 def index(request):
@@ -21,10 +29,11 @@ def subproblems_form(request,mainproblem_name,main_id,subproblem_name,sub_id):
         'mainproblem_name':mainproblem_name,
         'subproblem_name':subproblem_name,   
         'main_id':main_id,
+        'sub_id':sub_id,
     }
     return render(request,'socialShout/problemsform.html',params)
 
-def problems_form_submit(request):
+def problems_form_submit(request,main_id,sub_id):
     if request.method == 'POST':
         fullname = request.POST['fullname']
         inputaddress = request.POST['inputaddress']
@@ -34,17 +43,87 @@ def problems_form_submit(request):
         aadharno = request.POST['aadharno']
         pincode = request.POST['pincode']
         inputcity = request.POST['inputcity']
-        password = request.POST['state']
-        password = request.POST.get('complaint')
-        upload_file= request.FILES['file1'] if 'file' in request.FILES else False
-        upload_file= request.FILES['file1'] if 'file' in request.FILES else False
+        state = request.POST['state']
+        complaint = request.POST.get('complaint')
+        upload_file= request.FILES['file1'] if 'file1' in request.FILES else False
+        upload_file1= request.FILES['file2'] if 'file2' in request.FILES else False
 
+        digits = [i for i in range(0, 10)]
+        random_str = ""
+        for i in range(6):
+            index = math.floor(random.random() * 10)
+            random_str += str(digits[index])
+        try:
+            send_mail('Verfiy Your Complaint by enter OTP', 
+            'Your OTP for Verfiy Your Complaint is '+random_str+'.',
+                'admin@psmweb.in',
+                [email],
+                fail_silently=False)
+            cu1= Complaint_User()
+            cu1.complaint_category = Complaint_Category(main_id)
+            cu1.complaint_subcategory = Complaint_Subcategory(sub_id)
+            cu1.full_name =fullname
+            cu1.address =  inputaddress
+            cu1.email =  email
+            cu1.phone =  contactno1
+            cu1.alternative_phone =  contactno2
+            cu1.aadhar_no =  aadharno
+            cu1.pincode =  pincode
+            cu1.city =  inputcity
+            cu1.state =  state
+            cu1.complaint_info =  complaint
+            cu1.complaint_aadhar =  upload_file
+            cu1.complaint_image =  upload_file1
+            cu1.otp_code = random_str
+            cu1.otp_created_at = datetime.now()
+            cu1.save()
+            request.session['complaint_email'] = email
+            request.session['complaint__id'] = cu1.id
+            messages.success(request,"Email is sucessfully sent")
+            return redirect("/verify_otp/")
 
-
+        except Exception as e:
+            messages.error(request, e)
+#            problems_form/<str:mainproblem_name>/<int:main_id>/<str:subproblem_name>/<int:sub_id>/
+            return redirect("problems_form/mainproblem_name/"+str(main_id)+"/subproblem_name/"+str(sub_id)+"/")
+        
+        # try:
+        #     send_mail('Verfiy Your Complaint by enter OTP', 
+        #     'Your OTP for Verfiy Your Complaint is '+random_str+'.',
+        #         'admin@psmweb.in',
+        #         [email],
+        #         fail_silently=False)
+        # except Exception as e:
+        #     messages.error(request, e)
+        #     request.session['complaint_email'] = email
+        # return redirect("/verify_otp/"+str(cu1.id)+"/")
     return render(request,'socialShout/otp_verify.html',params)    
 
 def verify_otp(request):
-    params = {
+    complaint_email = request.session.get('complaint_email')
+    complaint_id = request.session.get('complaint__id')
+    if request.method == 'POST':
+        otp = request.POST['otp']
+        complaint_user = Complaint_User.objects.get(id = complaint_id)
+        time_limit =  complaint_user.otp_created_at + timedelta(minutes=10)
+        now_time =  datetime.utcnow()
+        if complaint_user.otp_created_at >= now_time: 
+            if complaint_user.otp_code == otp:
+                complaint_user.is_otp_verify = True
+                complaint_user.save()
+                messages.success(request,"Your complaint is sucessfully registered")
+                return redirect("/")
+            else:
+                messages.error(request, "Plese enter correct otp")
+                return redirect("/verify_otp/")
+        else:
+            messages.error(request, "Your otp is expire")
+            return redirect("/verify_otp/")
 
-    }
+        complaint_user = Complaint_User.objects.get(id=complaint_id)
+
+    params = {
+        'complaint_email':complaint_email,
+        'complaint_id':complaint_id,
+    } 
     return render(request,'socialShout/otp_verify.html',params)
