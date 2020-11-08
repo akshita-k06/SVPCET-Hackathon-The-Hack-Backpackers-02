4c6105b4ab3e59ad2c6f61c6aae1f3d71007a6a8
Pradyumna Mahajan <65710156+pradyumnamahajan52@users.noreply.github.com>
2020-11-08 10:10:42 +0530

    Add files via upload

---
+++ static/js/otp.js
@@ -0,0 +1,9 @@
+function logout() {
+    $('#logoutModal').modal('show')
+    // var logoutVal = confirm("Do you really want to logout ?");
+    // if (logoutVal == true) {
+    //  window.location.href = "/logout";
+  
+    // }
+    
+  }
